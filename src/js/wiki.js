b25a63e948ae323d3d18438ea048a5bf176c9b84
NewPort25 <demian.huber@proton.me>
2025-04-14 22:45:10 +0200

    asdfs

--- src/js/wiki.js
+++ src/js/wiki.js
@@ -537,10 +537,10 @@ function replaceRobogatorPlaceholdersWithEllipsis(text) {
 
 function replaceRobogatorPlaceholdersWithContent(text, images, code, color) {
     
-    // Show code
+    // Formate code
     text = text.replace(/ROBOCODE(\d+)/g, (match, number) => {return '<div class="account_standard_code_panel" style="background-color:' + COLORCODEBG + ';"><div class="account_standard_code" style="color: ' + color + ';" >' + code[number] + '</div></div>'});
 
-    // Show images
+    // Formate images
     text = text.replace(/ROBOIMAGE(\d+)/g, (match, number) => {return '<div class="mainThirdColor account_standard_image_panel"><img src="'+ 
         pathToArticleImages + image[number]  + '" ></div>'});
 
