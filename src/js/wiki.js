3188d6f6b6044e434a7c51ee486fe9515b8b3159
NewPort25 <demian.huber@proton.me>
2025-04-14 22:18:20 +0200

    sdfsdf

--- src/js/wiki.js
+++ src/js/wiki.js
@@ -537,11 +537,8 @@ function replaceRobogatorPlaceholdersWithEllipsis(text) {
 
 function replaceRobogatorPlaceholdersWithContent(text, images, code, color) {
     
-    text = text.replace(/ROBOCODE(\d+)/g, (match, number) => {'<div class="account_standard_code_panel" style="background-color:' + COLORCODEBG + ';"><div class="account_standard_code" style="color: ' + color + ';" >' + code[number] + '</div></div>' + alert(code[number]);});
+    text = text.replace(/ROBOCODE(\d+)/g, (match, number) => {'<div class="account_standard_code_panel" style="background-color:' + COLORCODEBG + ';"><div class="account_standard_code" style="color: ' + color + ';" >e</div></div>'});
 
-    
-    //text = text.replace(/ROBOIMAGE\d+/g, replaceString);
-    
     return text;
 }
 
