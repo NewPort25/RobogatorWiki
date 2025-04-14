fdee4f394870437565ca676e9efdf904c0ec7971
NewPort25 <demian.huber@proton.me>
2025-04-14 15:20:54 +0200

    zizui

--- src/js/wiki.js
+++ src/js/wiki.js
@@ -540,7 +540,7 @@ function replaceRobogatorPlaceholdersWithContent(text, images, code, color) {
     text = text.replace(/ROBOCODE(\d+)/g, (match, number) => {'<div class="account_standard_code_panel" style="background-color:' + COLORCODEBG + ';"><div class="account_standard_code" style="color: ' + color + ';" >' + code[number] + '</div></div>'});
 
 
-    text = text.replace(/ROBOIMAGE\d+/g, replaceString);
+    //text = text.replace(/ROBOIMAGE\d+/g, replaceString);
     
     return text;
 }
